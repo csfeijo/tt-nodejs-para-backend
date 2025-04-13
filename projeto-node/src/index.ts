8f5c57e5739514b07dc5b1f3838b137133c98d88
Cícero Feijó <cicero.feijo@gmail.com>
2025-04-12 23:59:01 -0300

    feat: pug templates

--- projeto-node/src/index.ts
+++ projeto-node/src/index.ts
@@ -1,52 +1,34 @@
 import express, { Request, Response } from "express";
+import path from "path";
 
 const app = express();
+// Adiciona o path de assets
+app.use(express.static("public"));
 // Habilita a leitura de dados em JSON no POST
 app.use(express.json());
 // Habilitar a leitura de dados vindos de formulários
 app.use(express.urlencoded({ extended: true }));
+// Configura para uso do PUG Templates
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "templates"));
+
 const port = 3030;
 
 app.get("/", (req: Request, res: Response): void => {
-  res.send("GET Hello World com Express usando TS!");
+  // HOME é o nome do arquivo dentro do diretório de templates
+
+  res.render("home", {
+    titulo: "Página em PUG",
+    nome: "Cícero Feijó",
+    idade: 12,
+    cidades: ["Alvorada", "Gravataí", "Porto Alegre", "Torres"],
+  });
 });
 
-// Tratando dados vindos via QueryString
 app.get("/clientes", (req: Request, res: Response): void => {
-  const { nome, idade } = req.query as { nome: string; idade: string };
-
-  res.send(`GET Clientes: ${nome} - ${parseInt(idade)}`);
-});
-
-// Tratando os dados vindos via parametros de URL
-app.get("/clientes/:id", (req: Request, res: Response): void => {
-  const { id } = req.params as { id: string };
-
-  res.send(`GET Clientes com parametro - id: ${id}`);
-});
-
-// Tratando os dados vindos via POST
-app.post("/clientes", (req: Request, res: Response): void => {
-  const { nome, idade, genero } = req.body as {
-    nome: string;
-    idade: string;
-    genero: string;
-  };
-  let mensagem = "";
-
-  if (parseInt(idade) >= 18) {
-    mensagem = "Já pode ter habilitação";
-  } else {
-    mensagem = "Ainda não pode ter habilitação";
-  }
-
-  let texto = "POST Clientes <br/>";
-  texto += `Nome: ${nome} <br/>`;
-  texto += `Idade: ${idade} <br/>`;
-  texto += `Genêro: ${genero} <br/>`;
-  texto += mensagem;
-
-  res.send(texto);
+  res.render("clientes", {
+    titulo: "Página de clientes",
+  });
 });
 
 app.listen(port, () => {

58b5e75852062c96c92569fe72e521778f64765a
Cícero Feijó <cicero.feijo@gmail.com>
2025-04-15 00:47:26 -0300

    feat: ajusta build

--- projeto-node/dist/index.js
+++ projeto-node/dist/index.js
@@ -1,2 +1,36 @@
-console.log("Executando TS com Node - com Sucesso!");
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const app = (0, express_1.default)();
+// Adiciona o path de assets
+app.use(express_1.default.static("public"));
+// Habilita a leitura de dados em JSON no POST
+app.use(express_1.default.json());
+// Habilitar a leitura de dados vindos de formulários
+app.use(express_1.default.urlencoded({ extended: true }));
+// Configura para uso do PUG Templates
+app.set("view engine", "pug");
+app.set("views", path_1.default.join(__dirname, "templates"));
+const port = 3030;
+app.get("/", (req, res) => {
+    // HOME é o nome do arquivo dentro do diretório de templates
+    res.render("home", {
+        titulo: "Página em PUG",
+        nome: "Cícero Feijó",
+        idade: 12,
+        cidades: ["Alvorada", "Gravataí", "Porto Alegre", "Torres"],
+    });
+});
+app.get("/clientes", (req, res) => {
+    res.render("clientes", {
+        titulo: "Página de clientes",
+    });
+});
+app.listen(port, () => {
+    console.log(`Servidor executando na porta ${port}`);
+});
 //# sourceMappingURL=index.js.map

cd10cec21c8b5fd9f1d549829952169422c8e923
Cícero Feijó <cicero.feijo@gmail.com>
2025-04-16 00:04:28 -0300

    feat: adiciona layout novo

--- projeto-node/dist/index.js
+++ projeto-node/dist/index.js
@@ -7,7 +7,9 @@ const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 // Adiciona o path de assets
-app.use(express_1.default.static("public"));
+//app.use(express.static("public"));
+//app.use(express.static(path.join(__dirname, "public")));
+app.use(express_1.default.static(path_1.default.resolve(process.cwd(), "public")));
 // Habilita a leitura de dados em JSON no POST
 app.use(express_1.default.json());
 // Habilitar a leitura de dados vindos de formulários
@@ -28,6 +30,19 @@ app.get("/", (req, res) => {
 app.get("/clientes", (req, res) => {
     res.render("clientes", {
         titulo: "Página de clientes",
+    });
+});
+app.get("/cadastro", (req, res) => {
+    res.render("cadastro", {
+        titulo: "Cadastro",
+    });
+});
+app.post("/cadastro", (req, res) => {
+    const { nome, email } = req.body;
+    res.render("cadastro-recebido", {
+        titulo: "Cadastro Recebido",
+        nome,
+        email,
     });
 });
 app.listen(port, () => {
--- projeto-node/src/index.ts
+++ projeto-node/src/index.ts
@@ -3,7 +3,10 @@ import path from "path";
 
 const app = express();
 // Adiciona o path de assets
-app.use(express.static("public"));
+//app.use(express.static("public"));
+//app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.resolve(process.cwd(), "public")));
+
 // Habilita a leitura de dados em JSON no POST
 app.use(express.json());
 // Habilitar a leitura de dados vindos de formulários
@@ -28,6 +31,21 @@ app.get("/", (req: Request, res: Response): void => {
 app.get("/clientes", (req: Request, res: Response): void => {
   res.render("clientes", {
     titulo: "Página de clientes",
+  });
+});
+
+app.get("/cadastro", (req: Request, res: Response): void => {
+  res.render("cadastro", {
+    titulo: "Cadastro",
+  });
+});
+
+app.post("/cadastro", (req: Request, res: Response): void => {
+  const { nome, email } = req.body as { nome: string; email: string };
+  res.render("cadastro-recebido", {
+    titulo: "Cadastro Recebido",
+    nome,
+    email,
   });
 });
 
